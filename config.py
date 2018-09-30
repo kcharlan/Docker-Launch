88e56310b73cc78f710af48bb5d271cb2246e165
kcharlan <kcharlan@gmail.com>
2018-09-30 00:05:36 -0400

    Forgot to clean up the config.py settings. Fixed now, and ready for real testing on a fresh platform.

--- config.py
+++ config.py
@@ -13,12 +13,8 @@ try:
 except ValueError:
 	sys.path.append(SOURCE_PATH)
 
-#INSTALLED_PATH = '/usr/local/sbin/docker-launch'
-INSTALLED_PATH = '/Users/kevinharlan/Documents/Development/test/testinstall/install'
+INSTALLED_PATH = '/usr/local/sbin/docker-launch'
 
-#CONFIG_FILE = '/etc/docker-launch.conf'
-CONFIG_FILE = '/Users/kevinharlan/Documents/Development/test/testinstall/etc/docker-launch.conf'
-#CONFIG_FILE = f'{SOURCE_PATH}/samples/sample-docker-launch.conf'
+CONFIG_FILE = '/etc/docker-launch.conf'
 
-#INIT_PATH = '/etc/init.d'
-INIT_PATH = '/Users/kevinharlan/Documents/Development/test/testinstall/etc/init.d'
+INIT_PATH = '/etc/init.d'
